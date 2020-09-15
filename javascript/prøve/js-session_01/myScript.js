27a904003809da9d6655c58e046e8993ebef414b
serdar <serdar83durmus@gmail.com>
2020-09-15 21:00:53 +0200

    noen methoder er legget

--- javascript/prøve/js-session_01/myScript.js
+++ javascript/prøve/js-session_01/myScript.js
@@ -89,3 +89,45 @@ console.log(myValue_1/myValue_2)
 console.log(typeof (myValue_1/myValue_2))
 console.log(myValue_1+myValue_3)
 console.log(typeof (myValue_1+myValue_3))
+
+console.log("/ ----------------------- /")
+// LENGTH
+const myString_1 = "Merhaba!";
+console.log("length of myString_1: " + myString_1.length);
+const myString_2 = ""
+console.log("length of myString_2: " + myString_2.length);
+
+// CONCAT
+const myString_3 = "Serdar"
+const myString_4 = "Durmus"
+console.log("concat: " + myString_3.concat(" ".concat(myString_4)));
+console.log("concat: " + myString_3.concat(" ", myString_4));
+
+// CHAR-AT
+console.log("");
+console.log("// CHAR-AT");
+const myString_5 = "SDs Firma"
+console.log(myString_5.charAt(1)) // D
+console.log(myString_5.charAt(17)) // ingen ting
+console.log(myString_5.charAt(-1)) // ingen ting
+
+// INCLUDES()
+console.log("");
+console.log("// INCLUDES()");
+const myString_6 = "Hello friends, Welcome to Home"
+console.log(myString_6.includes("to")) // True
+console.log(myString_6.includes("To")) // False
+
+// indexOf
+console.log("");
+console.log("// indexOf");
+const myString_7 = "Hello friends, Welcome to Home"
+console.log(myString_7.indexOf("ş")) // -1
+console.log(myString_7.indexOf("to Hom")) // 23
+console.log(myString_7.indexOf("t")) // 23
+
+// REPLACE
+console.log("");
+console.log("// REPLACE");
+const myString_8 = "Hello friends, Welcome to Home";
+console.log(myString_8.replace("Hello", "Hi"));
