7020b85a511841f4d08a18c864cc5889a30408e3
serdar <serdar83durmus@gmail.com>
2020-09-19 19:32:27 +0200

    js-session_04 Array er inne

---
+++ javascript/hands-on/js-session_04 Array/myScript.js
@@ -0,0 +1,19 @@
+// ARRAY
+
+// Flat array
+{
+    const myArray = ["Oslo", "Bergen", "Trondheim", 1,2,false];  
+    console.log("myArray: ", myArray);
+    console.log("length: ",myArray.length);
+}
+{
+    const myArray = ["Oslo", "Bergen", "Trondheim", 1,2,false];  
+    let myArray2 = ["Lier", "Drammen", "Asker"];
+    const result = myArray.concat(myArray2);
+    console.log(result);
+}
+
+
+// flat array
+// array of array
+// object of array
