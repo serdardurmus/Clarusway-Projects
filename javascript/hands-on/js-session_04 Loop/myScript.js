acc2b774cf367d5006b1fc7851b24ce109fdc899
serdar <serdar83durmus@gmail.com>
2020-09-19 19:21:14 +0200

    js-session_04 Loop er ferdig

--- javascript/hands-on/js-session_04 Loop/myScript.js
+++ javascript/hands-on/js-session_04 Loop/myScript.js
@@ -12,3 +12,54 @@ for (let i=0; i<myString.length; i++) {
     console.log(`${i + 1}.inci harf ${myString[i]}`)
 }
 
+// WHILE LOOPS
+{
+let i = 0;
+while(i<5) {
+    console.log(i);
+    i++;
+}
+}
+
+{
+    let i = 0, sum=0;
+while(i<5) {
+    sum += i;
+    i++;
+}
+console.log("Sum: ",sum);
+}
+
+// DO..WHILE LOOPS
+{
+    console.log("Do While Code:")
+    let i = 0;
+    do {
+        console.log(i);
+        i++;
+    } while (i>5);
+}
+
+// BREAK & CONTINIUE
+{
+    console.log("BREAK & CONTINIUE:")
+    for (let i = 0; i<10; i++) {
+        if (i===4) continue;
+        if (i===8) break;
+        console.log(i);
+    }
+}
+
+{
+    let myString = "Serdar", result = "";
+    for (let i = 0; i<myString.length; i++) {
+        if(myString[i] ==="e") continue;
+        if(myString[i] ==="f") break;
+        result += myString[i]
+    }
+    console.log(result);
+}
+
+{
+    
+}
