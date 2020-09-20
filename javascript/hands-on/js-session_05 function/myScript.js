f52970f6c18ae5a5d0303505a65d9821ee6093d6
serdar <serdar83durmus@gmail.com>
2020-09-20 15:20:37 +0200

    js-session_05 function er inne

---
+++ javascript/hands-on/js-session_05 function/myScript.js
@@ -0,0 +1,11 @@
+const myNumber_1 = 9;
+const myNumber_2 = 10;
+
+function sumNumbers () {
+    const result =myNumber_1 + myNumber_2
+    return result;
+    const abc= 9;
+}
+
+const res = sumNumbers();
+console.log(res);
