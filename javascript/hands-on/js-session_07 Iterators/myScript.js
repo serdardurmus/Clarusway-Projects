79ec8273b046b7a6439553c6f5d7cc35ba056451
serdar <serdar83durmus@gmail.com>
2020-09-26 17:58:50 +0200

    js-session_07 Iterators eks er inne

--- javascript/hands-on/js-session_07 Iterators/myScript.js
+++ javascript/hands-on/js-session_07 Iterators/myScript.js
@@ -13,7 +13,7 @@
     numbers.forEach(logNumbers);
     function logNumbers(number) {
         console.log("number:", number);
-}
+    }
 }
 
 // -----------------------------------------------
@@ -24,7 +24,7 @@ console.log("map:");
 
     const newNumbers = numbers.map(num => {
         // console.log("num:", num)
-        return num*2
+        return num * 2
     })
 
     console.log(newNumbers)
@@ -35,7 +35,7 @@ console.log("");
 console.log("map and forEach:");
 {  // map and forEach
     const numbers = [1, 3, 6, 7, 33, 2, 4, 10];
-    
+
     const num_1 = numbers.forEach((num) => console.log(num))
     const num_2 = numbers.map((num) => console.log(num))
 
@@ -52,11 +52,11 @@ console.log("Find:");
 // İlk bulduğunu getiriyor
 {
     const users = [
-        {id:0, name:"Jack", age:20},
-        {id:1, name:"Milton", age:34},
-        {id:2, name:"Arthur", age:40},
-        {id:3, name:"Mercedes", age:10},
-        {id:4, name:"David", age:34}
+        { id: 0, name: "Jack", age: 20 },
+        { id: 1, name: "Milton", age: 34 },
+        { id: 2, name: "Arthur", age: 40 },
+        { id: 3, name: "Mercedes", age: 10 },
+        { id: 4, name: "David", age: 34 }
     ];
 
     const myUser = users.find(user => user.age == 34);
@@ -71,13 +71,106 @@ console.log("Filter:");
 // filter
 {
     const users = [
-        {id:0, name:"Jack", age:20},
-        {id:1, name:"Milton", age:34},
-        {id:2, name:"Arthur", age:40},
-        {id:3, name:"Mercedes", age:10},
-        {id:4, name:"David", age:34}
+        { id: 0, name: "Jack", age: 20 },
+        { id: 1, name: "Milton", age: 34 },
+        { id: 2, name: "Arthur", age: 40 },
+        { id: 3, name: "Mercedes", age: 10 },
+        { id: 4, name: "David", age: 34 }
     ];
     const myUser1 = users.filter(user => user.age < 40);
     console.log(myUser1);
 }
 
+// -----------------------------------------------
+console.log("");
+console.log("Reduce:");
+// Reduce
+// acc: son yapılan return işlemini tutuyor
+// curr: şu andaki pozisyonun değerini veriyor
+
+{
+    const myArray = [1, 3, 15, 6, 8];
+    const max = myArray.reduce((acc, curr) => {
+        // console.log("acc:", acc, "curr:", curr);
+        if (acc < curr) { return curr } else { return acc }
+    });
+
+    console.log("max:", max)
+}
+
+{
+    const myArray = [1, 3, 15, 6, 8];
+
+    const max = myArray.reduce((acc, curr) => {
+        return acc > curr ? acc : curr;
+    });
+    console.log("max:", max)
+}
+
+
+// -----------------------------------------------
+console.log("");
+console.log("every and some:");
+// every - hepsi için sağlanması lazım sonra true döner
+// some - en az biri sağlasa da olur, sonra true döner
+
+{
+    const users = [
+        { id: 0, isOld: false, name: "Jack", age: 20 },
+        { id: 1, isOld: true, name: "Milton", age: 34 },
+        { id: 2, isOld: true, name: "Arthur", age: 40 },
+        { id: 3, isOld: false, name: "Mercedes", age: 10 },
+        { id: 4, isOld: true, name: "David", age: 34 }
+    ];
+
+    console.log("every: ", users.every(user => user.isOld === true));
+    console.log("some: ", users.some(user => user.isOld === true));
+}
+
+// -----------------------------------------------
+console.log("");
+console.log("for-in and for-of:");
+// for-in - 
+// for-of - 
+
+{
+    const users2 = { 
+        id: 0, 
+        isOld: false, 
+        name: "Jack", 
+        age: 20 };
+    for (key in users2) {
+        console.log("key:",key);
+        console.log("value:",users2[key]);
+    }
+
+
+    const users = [
+        { id: 0, isOld: false, name: "Jack", age: 20 },
+        { id: 1, isOld: true, name: "Milton", age: 34 },
+        { id: 2, isOld: true, name: "Arthur", age: 40 },
+        { id: 3, isOld: false, name: "Mercedes", age: 10 },
+        { id: 4, isOld: true, name: "David", age: 34 }
+    ];
+
+    for (element of users) {
+        console.log(element);
+    }
+}
+ // nested kan vi bruke.
+{
+    const users = [
+        { id: 0, isOld: false, name: "Jack", age: 20 },
+        { id: 1, isOld: true, name: "Milton", age: 34 },
+        { id: 2, isOld: true, name: "Arthur", age: 40 },
+        { id: 3, isOld: false, name: "Mercedes", age: 10 },
+        { id: 4, isOld: true, name: "David", age: 34 }
+    ];
+
+    for (element of users) {
+        for (key in element) {
+            console.log("key:",key);
+            console.log("value:",element[key]);
+        }
+    }
+}
---
+++ javascript/hands-on/js-session_07 Iterators/myScript_eks.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+      id: 0,
+      title: "Tempor aute aliqua reprehenderit aute anim ullamco anim excepteur ad id.",
+      desc: "Amet qui qui est sit culpa aliqua consequat in esse nisi mollit elit pariatur ipsum. Nulla do tempor ut sit qui. Laborum amet eiusmod Lorem dolor. Voluptate fugiat ipsum non ipsum proident reprehenderit adipisicing do anim ad cillum.",
+      readCount: 2310,
+    },
+    {
+      id: 1,
+      title: "Qui duis tempor qui eiusmod id tempor est ipsum anim ipsum sit.",
+      desc: "Qui proident culpa excepteur elit minim nostrud nostrud sit ea ad proident labore Lorem. Et et elit et sint nisi sint aliqua velit exercitation sint mollit labore. Commodo pariatur deserunt aliquip sit in reprehenderit occaecat excepteur fugiat amet quis labore tempor ex.",
+      readCount: 3200,
+    },
+    {
+      id: 2,
+      title: "Sint est sint pariatur minim ipsum pariatur laborum nulla est.",
+      desc: "Sit officia enim culpa voluptate enim amet. Sunt eiusmod elit sit ipsum. Ad consequat magna cillum ut tempor commodo.",
+      readCount: 1390,
+    },
+    {
+      id: 3,
+      title: "Incididunt duis pariatur aliquip dolore sint officia ea laboris excepteur. Officia sint cupidatat irure occaecat non ut deserunt. Sunt magna enim enim magna proident reprehenderit Lorem occaecat consequat qui. Reprehenderit laboris aute cillum ipsum ea ipsum non irure nulla commodo ipsum. Incididunt quis dolore aute in duis mollit. Consequat occaecat anim proident reprehenderit voluptate sint.",
+      desc: "Magna eiusmod ex incididunt Lorem ipsum commodo enim id incididunt quis. Ut reprehenderit elit consectetur deserunt est consequat sunt duis elit et et commodo qui. Eiusmod nulla aliqua excepteur Lorem reprehenderit dolor. Eiusmod sunt ut qui non culpa enim quis. Officia consequat cillum labore nulla labore ipsum exercitation ullamco mollit laborum Lorem. Lorem dolore aliquip ea cillum labore duis adipisicing ex sit reprehenderit.",
+      readCount: 3420,
+    },
+    {
+      id: 4,
+      title: "Eiusmod laborum exercitation elit eiusmod dolore sunt incididunt amet nisi minim ea cupidatat mollit.",
+      desc: "Do culpa ea occaecat pariatur amet magna. Elit culpa id voluptate reprehenderit nulla tempor eu adipisicing mollit. Voluptate magna nisi veniam esse elit. Ex ullamco culpa velit sint ex aute culpa eu ea do laboris non irure.",
+      readCount: 230,
+    },
+  ];
+
+  function loadArticles(ReadingCount) {
+      // console.log(ReadingCount)
+    let articeleData = data;
+    const articles = document.querySelector("#articles");
+
+    
+
+  data.forEach(d => {
+      const articleElement = document.createElement("div");
+      articleElement.classList.add("article");
+      articleElement.innerHTML = `
+      <p class="title">${d.title}</p>
+      <p class="">${d.desc}</p>
+      <p class="reading_count">Reading:${d.readCount}</p>
+      `;
+      // console.log(articleElement);
+
+      articles.appendChild(articleElement);
+  });
+}
+
+function filterArticles() {
+    const countValue = document.querySelector("#count_input").value;
+    loadArticles(countValue);
+}
+
+document
+.querySelector("#search_button")
+.addEventListener("click", filterArticles);
+
+loadArticles();
+  // console.log(articles);
