0bbaa2568ea9e9265b1552ac4a7679aa30138feb
Serdar <serdar83durmus@gmail.com>
2020-12-05 16:47:44 +0100

    npmi cookie-parser

--- nodejs/hands-on/session5_middleware/index.js
+++ nodejs/hands-on/session5_middleware/index.js
@@ -1,14 +1,33 @@
 const express = require("express");
 require("dotenv").config();
+var cookieParser = require('cookie-parser')
+
 const logger = require("morgan");
 const port = process.env.port || 5001;
 const host = "localhost";
 const app = express();
 //middleware
-app.use(logger("dev"));
-app.get("/user", (req, res) => {
-  res.send("User details");
-});
-app.listen(port, host, () => {
-  console.log("I'm listening on http://%s:%s", host, port);
-});
+
+app.use(cookieParser());
+
+app.use((req, res, next) => { //Authentication
+    console.log("Auth");
+    // some jobs
+    res.isAuth = false;
+    next();
+  });
+  app.use((req, res, next) => {
+    console.log("Controller // db");
+   if (res.isAuth) {
+       console.log("Private data")
+   } else {
+       console.log("not auth")
+   }
+    next();
+  });
+  app.use((req, res, next) => {
+    console.log("middleware: 3");
+    if (true) {
+      res.send("send");
+    }
+  });
