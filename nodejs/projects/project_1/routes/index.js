80c19bac25f9a5c531789499e899b5ff5b297cf6
Serdar <serdar83durmus@gmail.com>
2020-12-05 19:58:25 +0100

    Update homepage - users table view

--- nodejs/projects/project_1/routes/index.js
+++ nodejs/projects/project_1/routes/index.js
@@ -1,9 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const indexController = require("../controllers/indexController");
 
 /* GET home page. */
-router.get("/", indexController.get_index);
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
