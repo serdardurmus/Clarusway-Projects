8d714c21d5dfe18be9a4ea8ae7f1a71ab7049c1e
Serdar <serdar83durmus@gmail.com>
2020-12-05 16:28:41 +0100

    add logger

--- nodejs/hands-on/session5_middleware/index.js
+++ nodejs/hands-on/session5_middleware/index.js
@@ -1,31 +1,14 @@
-// middleware neden kullanılır?
-
 const express = require("express");
 require("dotenv").config();
+const logger = require("morgan");
 const port = process.env.port || 5001;
 const host = "localhost";
 const app = express();
 //middleware
-app.use((req, res, next) => {
-  console.log("session expiry check");
-  // some jobs
-  next();
+app.use(logger("dev"));
+app.get("/user", (req, res) => {
+  res.send("User details");
 });
-app.use((req, res, next) => {
-  console.log("user auth");
-  next();
-});
-app.use((req, res, next) => {
-    console.log("cookies");
-    next();
-  });
-app.use((req, res, next) => {
-  console.log("cookies");
-  if (true) {
-    res.send("OK");
-  }
-});
-app.get("/user", (req, res) => {});
 app.listen(port, host, () => {
   console.log("I'm listening on http://%s:%s", host, port);
 });
