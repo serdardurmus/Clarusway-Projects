b532ef3a9ece3ff93c92e1ab4143478e54fa503e
Serdar <serdar83durmus@gmail.com>
2020-12-05 17:31:50 +0100

    session5 er ferdig

--- nodejs/hands-on/session5_middleware/index.js
+++ nodejs/hands-on/session5_middleware/index.js
@@ -1,38 +1,16 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
-var cookieParser = require('cookie-parser')
-
-const logger = require("morgan");
-const port = process.env.port || 5001;
+const port = process.env.port || 8080;
 const host = "localhost";
 const app = express();
 //middleware
-
-app.use(cookieParser());
-
-app.use((req, res, next) => {
-    console.log("Cookies: ", req.signedCookies);
-    next();
-  });
-  
-app.use((req, res, next) => { //Authentication
-    console.log("Auth");
-    // some jobs
-    res.isAuth = false;
-    next();
-  });
-  app.use((req, res, next) => {
-    console.log("Controller // db");
-   if (res.isAuth) {
-       console.log("Private data")
-   } else {
-       console.log("not auth")
-   }
-    next();
-  });
-  app.use((req, res, next) => {
-    console.log("middleware: 3");
-    if (true) {
-      res.send("send");
-    }
-  });
+app.use(express.static(__dirname + "/public"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.listen(port, host, () => {
+    console.log("I'm listening on http://%s:%s", host, port);
+})
---
+++ nodejs/hands-on/session5_middleware/index2.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require("dotenv").config();
+var cookieParser = require('cookie-parser')
+
+const logger = require("morgan");
+const port = process.env.port || 5001;
+const host = "localhost";
+const app = express();
+//middleware
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    console.log("Cookies: ", req.signedCookies);
+    next();
+  });
+  
+app.use((req, res, next) => { //Authentication
+    console.log("Auth");
+    // some jobs
+    res.isAuth = false;
+    next();
+  });
+  app.use((req, res, next) => {
+    console.log("Controller // db");
+   if (res.isAuth) {
+       console.log("Private data")
+   } else {
+       console.log("not auth")
+   }
+    next();
+  });
+  app.use((req, res, next) => {
+    console.log("middleware: 3");
+    if (true) {
+      res.send("send");
+    }
+  });
