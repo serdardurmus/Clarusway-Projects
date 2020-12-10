d2363bfbbfa805399646aabbcebfa696af160163
Serdar <serdar83durmus@gmail.com>
2020-12-10 21:46:06 +0100

    Add user model

--- nodejs/projects/MERN/models/connectDB.js
+++ nodejs/projects/MERN/models/connectDB.js
@@ -1,1 +1,16 @@
-// mongodb+srv://dbUser:<password>@cluster0.pky1l.mongodb.net/test
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+      console.log(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Successfully connected to DB");
+  } catch (error) {
+    console.log("Error connecting DB", error);
+  }
+};
+
+module.exports = connectDB;
