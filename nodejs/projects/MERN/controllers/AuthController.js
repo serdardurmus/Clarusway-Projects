778270118b0e0d2b4f35fb4eac6263feb0944bdb
Serdar <serdar83durmus@gmail.com>
2020-12-10 21:59:23 +0100

    Add auth register todo

--- nodejs/projects/MERN/controllers/AuthController.js
+++ nodejs/projects/MERN/controllers/AuthController.js
@@ -1,10 +1,22 @@
 exports.authRegister = (req, res) => {
-    //TODO: Register func.
-    res.send("Register Completed.");
-  };
-  
-  exports.authLogin = (req, res) => {
-    // TODO: Auth.
-    // TODO: Login func.
-    res.send("Login Completed");
-  };
+  const { firstName, lastName, email, password } = req.body;
+  console.log(
+    "Fields:",
+    firstName,
+    lastName,
+    email,
+    password
+  );
+  // TODO1: Validate the fields
+  // TODO2: check already registered
+  // TODO3: crpyt password
+  // TODO4: save the user to DB
+
+  res.send("Register Completed.");
+};
+
+exports.authLogin = (req, res) => {
+  // TODO: Auth.
+  // TODO: Login func.
+  res.send("Login Completed");
+};
--- nodejs/projects/MERN/models/connectDB.js
+++ nodejs/projects/MERN/models/connectDB.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-      console.log(process.env.MONGODB_URI)
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
