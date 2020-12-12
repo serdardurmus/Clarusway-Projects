3bba949ea32ff0ef0f8d12a6555bfd5e11a5be2e
Serdar <serdar83durmus@gmail.com>
2020-12-12 17:47:25 +0100

    Add express validator

--- nodejs/projects/MERN/controllers/AuthController.js
+++ nodejs/projects/MERN/controllers/AuthController.js
@@ -3,39 +3,43 @@
   // TODO3: crpyt password (bcryptjs)
   // TODO4: save the user to DB  // done
 
-  const User = require("../models/UserModel");
-  const bcrypt = require("bcryptjs");
-  
-  exports.authRegister = async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-  
-    const userData = await User.findOne({ email });
-  
-    if (userData) {
-      return res
-        .status(400)
-        .json({ errors: [{ message: "User already exists!!" }] });
-    }
-  
-    const salt = await bcrypt.genSalt(10);
-  
-    const newPassword = await bcrypt.hash(password, salt);
-  
-    const user = new User({
-      firstName,
-      lastName,
-      email,
-      password: newPassword,
-    });
-  
-    await user.save();
-  
-    //TODO: Error handling for saving
-    res.send("Register Completed.");
-  };
-  
-  exports.authLogin = (req, res) => {
-    // TODO: Auth.
-    // TODO: Login func.
-    res.send("Login Completed");
-  };
+const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+exports.authRegister = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const validationErr = validationResult(req);
+
+  if (validationErr?.errors?.length > 0) {
+    return res.status(400).json({ errors: validationErr.array() });
+  }
+
+  const userData = await User.findOne({ email });
+  if (userData) {
+    return res
+      .status(400)
+      .json({ errors: [{ message: "User already exists!!" }] });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: newPassword,
+  });
+  await user.save();
+
+  //TODO: Error handling for saving
+  res.send("Register Completed.");
+};
+
+exports.authLogin = (req, res) => {
+  // TODO: Auth.
+  // TODO: Login func.
+  res.send("Login Completed");
+};
