c799cf0dd1d04ef972aaa5e89bf694998570f427
Serdar <serdar83durmus@gmail.com>
2020-12-26 19:56:12 +0100

    student_delete er  inne

--- django/hands-on/session4/src/fscohort/urls.py
+++ django/hands-on/session4/src/fscohort/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_view, student_list, student_add, student_detail
+from .views import home_view, student_list, student_add, student_detail, student_delete
 
 urlpatterns = [
     path("", home_view, name="home"),
     path("list/", student_list, name="list"),
     path("add/", student_add, name="add"),
     path("<int:id>", student_detail, name="detail"),
+    path("<int:id>/delete", student_delete, name="delete"),
     # path("about/", about)
 ]
