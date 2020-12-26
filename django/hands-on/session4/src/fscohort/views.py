31c32721a52ea8fd4335b30088d2685539f37e8e
Serdar <serdar83durmus@gmail.com>
2020-12-26 16:33:46 +0100

    templates er inne

--- django/hands-on/session4/src/fscohort/views.py
+++ django/hands-on/session4/src/fscohort/views.py
@@ -3,7 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return HttpResponse("Hello world.")
+    # print(request.GET.get("q"))
+    # print(request.user)
+    # print(request.path)
+    # print(request.method)
+    # print(request.COOKIES)
+    # return HttpResponse("Hello world.")
+    my_context = {
+        'title': 'clarusway',
+        'dict_1': { 'django': 'best framework'},
+        'my_list': [1,2,3,4,5]
+    }
+    return render(request, "fscohort/home.html", my_context)
 
 def about(request):
     return HttpResponse("About page")
